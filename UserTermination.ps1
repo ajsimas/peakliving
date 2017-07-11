67d974e12e6421412423ecba7658852bc4073f1d
ajsimas <me@ajsimas.com>
2017-07-11 14:31:02 -0600

    Adjusted script to work with LT
    removed all remnants of Import-Csv

--- UserTermination.ps1
+++ UserTermination.ps1
@@ -1,4 +1,4 @@
-﻿Set-ExecutionPolicy RemoteSigned
+Set-ExecutionPolicy RemoteSigned
 $Username = "itnow@peakliving.com"
 $Pass = "$Credential"
 $Password = $Pass | ConvertTo-SecureString -AsPlainText -Force
@@ -7,45 +7,49 @@ $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri ht
 Import-PSSession $Session
 Connect-MsolService -Credential $UserCredential
 
-Import-Csv C:\scripts\ToBeTerminated.csv | ForEach-Object{
-    $FirstName = Get-MsolUser -UserPrincipalName $_.UserPrincipalName | select -ExpandProperty FirstName
-    $LastName = Get-MsolUser -UserPrincipalName $_.UserPrincipalName | select -ExpandProperty LastName
-    $NewUPN = $FirstName + $LastName + '.' + $_.UserPrincipalName
-    $NewPrimarySmtp = 'SMTP:' + $NewUPN
-    $Forward = $_.Forward
-    $OldPrimarySmtp = 'smtp:' + $_.UserPrincipalName
-    Write-Output $_.UserPrincipalName
-    Write-Output $NewUPN
-    Write-Output $OldPrimarySmtp
-    
-    Set-MsolUserPrincipalName -UserPrincipalName $_.UserPrincipalName -NewUserPrincipalName $NewUPN
-    Set-Mailbox -Identity $_.UserPrincipalName -EmailAddress $NewPrimarySmtp -Type Shared -HiddenFromAddressListsEnabled $True
-    
-    Start-Sleep 120
+Param (
 
-    $UserPrincipalName = $NewUPN
-    $EmailAddresses = get-mailbox $UserPrincipalName | select -ExpandProperty emailaddresses
-    $EmailAddresses
-    $EmailAddresses | ForEach-Object{
-        if ($_ -ne $NewPrimarySmtp)
-        {
-            Write-Output 'removing email address'
-            Write-Output $_
-            Set-Mailbox -Identity $UserPrincipalName -EmailAddresses @{remove=$_}
-        }
-        }
+    [Parameter(Mandatory=$true)][string]$UserPrincipalName,
+    [Parameter(Mandatory=$true)][string]$Manager
 
-    $Licenses = Get-MsolUser -UserPrincipalName $NewUPN | select -ExpandProperty licenses
-    $Licenses
-    $Licenses | ForEach-Object{
-        Set-MsolUserLicense -UserPrincipalName $NewUPN -RemoveLicenses $_.AccountSkuId}
-    
-    If ($Forward)
+)
+
+$FirstName = Get-MsolUser -UserPrincipalName $UserPrincipalName | select -ExpandProperty FirstName
+$LastName = Get-MsolUser -UserPrincipalName $UserPrincipalName | select -ExpandProperty LastName
+$NewUPN = $FirstName + $LastName + '.' + $UserPrincipalName
+$NewPrimarySmtp = 'SMTP:' + $NewUPN
+$OldPrimarySmtp = 'smtp:' + $UserPrincipalName
+Write-Output $UserPrincipalName
+Write-Output $NewUPN
+Write-Output $OldPrimarySmtp
+
+Set-MsolUserPrincipalName -UserPrincipalName $UserPrincipalName -NewUserPrincipalName $NewUPN
+Set-Mailbox -Identity $UserPrincipalName -EmailAddress $NewPrimarySmtp -Type Shared -HiddenFromAddressListsEnabled $True
+
+Start-Sleep 120
+
+$UserPrincipalName = $NewUPN
+$EmailAddresses = get-mailbox $UserPrincipalName | select -ExpandProperty emailaddresses
+$EmailAddresses
+$EmailAddresses | ForEach-Object{
+    if ($_ -ne $NewPrimarySmtp)
     {
-        Start-Sleep 5
+        Write-Output 'removing email address'
+        Write-Output $_
+        Set-Mailbox -Identity $UserPrincipalName -EmailAddresses @{remove=$_}
+    }
+    }
 
-        Set-Mailbox -Identity $Forward -EmailAddresses @{add=$OldPrimarySmtp}
-    }
-    }
+$Licenses = Get-MsolUser -UserPrincipalName $NewUPN | select -ExpandProperty licenses
+$Licenses
+$Licenses | ForEach-Object{
+    Set-MsolUserLicense -UserPrincipalName $NewUPN -RemoveLicenses $_.AccountSkuId}
+
+If ($Manager)
+{
+    Start-Sleep 5
+
+    Set-Mailbox -Identity $Manager -EmailAddresses @{add=$OldPrimarySmtp}
+}
 
 Remove-PSSession $Session
